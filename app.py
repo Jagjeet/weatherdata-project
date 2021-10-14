c5e28ccba0d6c8fa3a201bb8a3e37c004b06da93
Robert-W2019 <robertresume18@gmail.com>
2021-10-13 19:00:28 -0700

    add csv

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# TODO
+# Use PyMongo to establish Mongo connection
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/weatherdata")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/store_inventory")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    #TODO - Retrieve real data from DB
+    weather_data = {}
+    weather_data['data'] = 'It sure is cold!'
+    # Find one record of data from the mongo database
+    # weatherdata = mongo.db.collection.find_one()
+
+
+    # Return template and data
+    return render_template("index.html", weather_data=weather_data)
+
+# TODO - Update routes below
+@app.route('/<my_type>/data')
+def db_data(my_type):
+
+    db_data = mongo.db.produce.find({'type':my_type}, {'_id': False})
+    print('this route was pinged')
+    parsed = [x for x in db_data]
+    print('parsed: ', parsed)
+    return jsonify(parsed)
+
+@app.route('/cost-gt/<cost>')
+def db_cost(cost):
+
+    db_data = mongo.db.produce.find({'cost': {'$gt': float(cost)}}, {'_id': False})
+    parsed = [x for x in db_data]
+    print('parsed: ', parsed)
+    return jsonify(parsed)
+
+if __name__ == '__main__':
+    app.run(debug=True)
